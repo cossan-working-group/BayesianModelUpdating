95af2edaddece9ce354c857be6f6dc4965b0325d
Adolphus Lye <46358970+Adolphus8@users.noreply.github.com>
2023-06-13 21:14:42 +0800

    Add files via upload

--- 18D DLR_AIRMOD Problem/AIRMOD_Bayesian_part1b_metamodel.m
+++ 18D DLR_AIRMOD Problem/AIRMOD_Bayesian_part1b_metamodel.m
@@ -1,3 +1,84 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d4ead4ed14797d3b74ce4c7bb3bd3fadafea56a124ae7589c82639baa6088efd
-size 3367
+Spath = fileparts(which('AIRMOD_Bayesian_part2_model_updating.m'));
+OpenCossan.setWorkingPath(fullfile(Spath,'workfolder'));
+
+%% Experimental data
+
+FE_frequencies = [0.29 0.56 0.82 2.14 5.6500 15.1100 33.3100 33.6200 35.3900 44.6600 47.2100 52.9100...
+    60.5900 67.6900 102.5900 128.6200 132.0800 145.9100 206.7300 225.7300 ....
+    261.5300 262.6400 278.7100 320.1500 321.6400 324.1200 336.3100 341.1500...
+    343.5500 359.5400];
+
+test_frequencies = [0.23 0.65 0.83 2.17 5.50,14.91,31.96,32.33,34.38,43.89,46.71,51.88,58.59,...
+    65.93,100.05,124.56,129.38,141.47,205.59,219.07,254.73,255.02,272.08,...
+    303.96,304.32,313.68,328.55,331.18,336.21,348.68];
+
+%% Load model
+
+load(fullfile(Spath,'fullmodel.mat'))
+Xinput = Xmodel.Xinput;
+Xinput = Xinput.sample('Nsamples',1);
+
+Nmodes = 30; % nr. of extracted modes
+CeigfNames = cell(1,Nmodes);
+for i = 1:Nmodes
+    CeigfNames{i} = ['freq' num2str(i)];
+end
+
+%% Load Latin Hypercube Sampling results from: "AIRMOD_Bayesian_part1a_LHS.m" file
+
+Xout_LHS = SimulationData.load('SfileName',fullfile(SbatchFolder,['SimulationData_batch_1_of_' num2str(Xlhs.Nbatches) '.mat']));
+for i=1:Xlhs.Nbatches
+    Xout_LHS = Xout_LHS.merge(SimulationData.load('SfileName',fullfile(SbatchFolder,['SimulationData_batch_' num2str(i) '_of_' num2str(Xlhs.Nbatches) '.mat'])));
+end
+Xout_LHS = Xout_LHS.removeData('Sname','eigenfrequencies'); % To remove Dataseries to increase computational speed
+
+%% Separate data in calibration and validation:
+
+Minput = Xout_LHS.getValues('CSnames',Xmodel.Cinputnames);
+Toutput = Xout_LHS.Tvalues;
+NcalibrationSamples = floor(Xout_LHS.Nsamples*0.8);
+
+% Assign input and output data
+XcalibrationInput = Xinput; XvalidationInput = Xinput;
+XcalibrationInput.Xsamples.MsamplesPhysicalSpace = Minput(1:NcalibrationSamples,:);
+XvalidationInput.Xsamples.MsamplesPhysicalSpace = Minput(NcalibrationSamples:end,:);
+XcalibrationOutput = Xout_LHS; XvalidationOutput = Xout_LHS;
+XcalibrationOutput.Tvalues = Toutput(1:NcalibrationSamples);
+XvalidationOutput.Tvalues = Toutput(NcalibrationSamples:end);
+
+%% Set-up the Artificial Neural Network:
+
+% 14 ANNs are set-up (for the 14 active eigenfrequencies). 
+% Each ANN trained to predict an specific active eigenfrequency.
+
+% The neural network has 4 layers: 1 input layer, 2 hidden-layers, and 1
+% output layer. The input layer consists of 18 nodes, the first
+% hidden-layer consists of 16 nodes, the second hidden-layer consists of 6
+% nodes, and the output layer consists of 1 node.
+
+Xnn(Nmodes) = NeuralNetwork;
+Xnn_no_data(Nmodes) = NeuralNetwork;
+if isempty(gcp)
+    parpool(12)
+end
+parfor imode = 1:Nmodes
+    Xnn(imode) = NeuralNetwork('Stype','HyperbolicTangent',...
+        'VhiddenNodes',[16 6],...
+        'XFullModel',Xmodel,...
+        'Cinputnames',Xmodel.Cinputnames,...
+        'Coutputnames',CeigfNames(imode),...
+        'XcalibrationInput',XcalibrationInput,...
+        'XcalibrationOutput',XcalibrationOutput,...
+        'XvalidationInput',XvalidationInput,...
+        'XvalidationOutput',XvalidationOutput);
+    Xnn(imode) = Xnn(imode).calibrate();
+    Xnn(imode) = Xnn(imode).validate();
+    Xnn_no_data(imode) = Xnn(imode);
+    Xnn_no_data(imode).XcalibrationInput=[];Xnn_no_data(imode).XcalibrationOutput=[];
+    Xnn_no_data(imode).XvalidationInput=[];Xnn_no_data(imode).XvalidationOutput=[];
+end
+
+%% Save the ANN Metamodel data:
+
+save metamodel.mat Xnn 
+save metamodel_light Xnn_no_data
--- 18D DLR_AIRMOD Problem/Airmod_log_p_D_theta.m
+++ 18D DLR_AIRMOD Problem/Airmod_log_p_D_theta.m
@@ -1,3 +1,62 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:85fa0faa02143a9b0b2914fad6f9a16e3be58e92092e6be9bc5bf1ca2a35f1ef
-size 1837
+function logL = Airmod_log_p_D_theta(D, theta, Xnn_no_data, selected_modes)
+% Calculation of the log_likelihood for the Airmod structure
+%
+% USAGE:
+% logL = problemA_log_p_D_theta(D, theta)
+%
+% INPUTS:
+% D     = experimental observations   nobs x dim_x
+% theta = epistemic parameters        npar x dim_theta
+%
+% OUTPUTS:
+% logL(i)  = loglikelihood for the set of parameters theta(i,:) and the
+%            data D, i = 1, 2, ...npar.        logL = npar x 1
+
+%%
+npar = size(theta,1);  % number of thetas to evaluate
+logL = zeros(npar,1);
+
+% call the model once for all the samples
+
+% load('metamodel_light','Xnn_no_data');
+
+%% Set input values
+Tinput = cell2struct(num2cell(theta(:,1:18)),Xnn_no_data(1).Cinputnames,2);
+%% Map these simulations through the system
+output = zeros(length(Tinput),length(selected_modes));
+for imode = 1:size(D,2)
+    Xout = Xnn_no_data(selected_modes(imode)).apply(Tinput);
+    output(:,imode)  = Xout.getValues('CSnames',Xnn_no_data(selected_modes(imode)).Coutputnames(1));
+end
+
+%% compute loglikelihood of each sample
+for ipar = 1:npar
+    logL(ipar) = sum((p_x_theta_pdf(D, output(ipar,:), theta(ipar,:))));
+    if isinf(logL(ipar))
+        logL(ipar) = -1e10;
+    end
+end
+
+end
+
+%%
+function p = p_x_theta_pdf(w, w_model, theta_i)
+% x       = set of observations         nobs x dim_x
+% theta_i = point in epistemic space    nsamples x dim_theta
+
+fixed_variance = 0;
+
+if ~fixed_variance
+    epsilon_r = theta_i(:,19:end); % std multiplied by 1000000 for numerical reasons
+else
+    epsilon_r = 1e-4*ones(size(theta_i));
+end
+p = zeros(size(theta_i,1),size(w_model,2));
+%% Estimate the PDF p_x_theta_pdf(x | theta)
+% compute likelihood accordind to the GOCE paper
+% p(i) = p_x_theta_pdf(x(i,:) | theta)
+for i = 1:length(w_model)
+    p(i) = (-1/2*nansum(((1-w(:,i).^2/w_model(i)^2)/epsilon_r(i)).^2,1));
+end
+
+end
